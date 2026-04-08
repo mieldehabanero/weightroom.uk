b995b7c05976686b83bea10a0a5aae9a1ed284b1
Ricardo Zamora <12755841+mieldehabanero@users.noreply.github.com>
2026-04-09 00:22:18 +0200

    Fix PHPUnit bootstrap and landing page test

--- tests/ExampleTest.php
+++ tests/ExampleTest.php
@@ -1,19 +1,11 @@
 <?php
 
-use Illuminate\Foundation\Testing\WithoutMiddleware;
-use Illuminate\Foundation\Testing\DatabaseMigrations;
-use Illuminate\Foundation\Testing\DatabaseTransactions;
-
 class ExampleTest extends TestCase
 {
-    /**
-     * A basic functional test example.
-     *
-     * @return void
-     */
     public function testBasicExample()
     {
-        $this->visit('/')
-             ->see('Laravel 5');
+        $this->get('/')
+            ->assertStatus(200)
+            ->assertSee('Track and analyse your training.');
     }
 }
