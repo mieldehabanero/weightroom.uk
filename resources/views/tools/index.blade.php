e1d62bb881e6de60956991ec7858b71c1d52d6e5
Ricardo Zamora <12755841+mieldehabanero@users.noreply.github.com>
2026-04-08 23:47:54 +0200

    Remove premium requirement for workout export

--- resources/views/tools/index.blade.php
+++ resources/views/tools/index.blade.php
@@ -83,7 +83,7 @@
     <p>Import workouts via CSV</p>
   </div>
   <div class="col-md-4">
-    <a href="{{ route('export') }}">Export workouts</a>@if (!Auth::user()->subscribed('weightroom_gold')) <a href="{{ route('userPremium') }}" class="text-muted" data-toggle="tooltip" data-placement="right" title="Premium Feature"><span class="glyphicon glyphicon-lock" aria-hidden="true"></span></a> @endif
+    <a href="{{ route('export') }}">Export workouts</a>
     <p>Export your workout data</p>
   </div>
 </div>
